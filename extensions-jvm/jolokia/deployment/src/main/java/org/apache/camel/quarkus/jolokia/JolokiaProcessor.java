13d53b5dd84b0f2b6e581323f6c4450abb58f17d
James Netherton <jamesnetherton@gmail.com>
2024-09-20 13:24:27 +0100

    WIP

---
+++ extensions-jvm/jolokia/deployment/src/main/java/org/apache/camel/quarkus/jolokia/JolokiaProcessor.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.jolokia;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
+import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.vertx.http.deployment.BodyHandlerBuildItem;
+import io.quarkus.vertx.http.deployment.NonApplicationRootPathBuildItem;
+import io.quarkus.vertx.http.deployment.RouteBuildItem;
+import org.jolokia.server.core.http.HttpRequestHandler;
+import org.jolokia.server.core.service.api.JolokiaContext;
+
+public class JolokiaProcessor {
+    private static final String FEATURE = "camel-jolokia";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    JolokiaContextBuildItem startJolokiaServiceManager(
+            ShutdownContextBuildItem shutdownContextBuildItem,
+            CamelJolokiaRecorder recorder) {
+        RuntimeValue<JolokiaContext> jolokiaContext = recorder.startJolokiaServiceManager(shutdownContextBuildItem);
+        return new JolokiaContextBuildItem(jolokiaContext);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    JolokiaHttpRequestHandlerBuildItem createJolokiaHttpRequestHandler(
+            JolokiaContextBuildItem jolokiaContext,
+            CamelJolokiaRecorder recorder) {
+        RuntimeValue<HttpRequestHandler> jolokiaHttpRequestHandler = recorder
+                .createJolokiaHttpRequestHandler(jolokiaContext.getRuntimeValue());
+        return new JolokiaHttpRequestHandlerBuildItem(jolokiaHttpRequestHandler);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void createManagementRoute(
+            JolokiaHttpRequestHandlerBuildItem jolokiaHttpRequestHandler,
+            NonApplicationRootPathBuildItem nonApplicationRootPathBuildItem,
+            BodyHandlerBuildItem bodyHandler,
+            BuildProducer<RouteBuildItem> routes,
+            CamelJolokiaRecorder recorder) {
+
+        routes.produce(nonApplicationRootPathBuildItem.routeBuilder()
+                .management()
+                .routeFunction("jolokia/*", recorder.route(bodyHandler.getHandler()))
+                .handler(recorder.getHandler(jolokiaHttpRequestHandler.getHandler()))
+                .blockingRoute()
+                .build());
+    }
+}
---
+++ extensions-jvm/jolokia/runtime/src/main/java/org/apache/camel/quarkus/jolokia/CamelJolokiaRecorder.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.jolokia;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.ShutdownContext;
+import io.quarkus.runtime.annotations.Recorder;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.RoutingContext;
+import org.jboss.logging.Logger;
+import org.jolokia.server.core.config.ConfigKey;
+import org.jolokia.server.core.config.StaticConfiguration;
+import org.jolokia.server.core.http.HttpRequestHandler;
+import org.jolokia.server.core.restrictor.AllowAllRestrictor;
+import org.jolokia.server.core.restrictor.DenyAllRestrictor;
+import org.jolokia.server.core.restrictor.RestrictorFactory;
+import org.jolokia.server.core.service.JolokiaServiceManagerFactory;
+import org.jolokia.server.core.service.api.JolokiaContext;
+import org.jolokia.server.core.service.api.JolokiaServiceManager;
+import org.jolokia.server.core.service.api.LogHandler;
+import org.jolokia.server.core.service.api.Restrictor;
+import org.jolokia.server.core.util.NetworkUtil;
+import org.jolokia.service.jmx.LocalRequestHandler;
+import org.jolokia.service.serializer.JolokiaSerializer;
+
+@Recorder
+public class CamelJolokiaRecorder {
+    private static final Logger LOG = Logger.getLogger(JolokiaHandler.class);
+
+    public Consumer<Route> route(Handler<RoutingContext> bodyHandler) {
+        return new Consumer<Route>() {
+            @Override
+            public void accept(Route route) {
+                route.handler(bodyHandler).produces("application/json");
+            }
+        };
+    }
+
+    public Handler<RoutingContext> getHandler(RuntimeValue<HttpRequestHandler> httpRequestHandler) {
+        return new JolokiaHandler(httpRequestHandler.getValue());
+    }
+
+    public RuntimeValue<JolokiaContext> startJolokiaServiceManager(ShutdownContext shutdownContext) {
+        StaticConfiguration configuration = new StaticConfiguration(ConfigKey.AGENT_ID,
+                NetworkUtil.getAgentId(hashCode(), "camel-quarkus-jolokia"));
+        String policyLocation = NetworkUtil.replaceExpression(configuration.getConfig(ConfigKey.POLICY_LOCATION));
+        JolokiaServiceManager serviceManager = JolokiaServiceManagerFactory.createJolokiaServiceManager(
+                configuration,
+                new CamelQuarkusJolokiaLogHandler(),
+                CamelQuarkusJolokiaRestrictor.resolve(policyLocation));
+        serviceManager.addService(new JolokiaSerializer());
+        serviceManager.addService(new LocalRequestHandler(1));
+        shutdownContext.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                serviceManager.stop();
+            }
+        });
+        return new RuntimeValue<>(serviceManager.start());
+    }
+
+    public RuntimeValue<HttpRequestHandler> createJolokiaHttpRequestHandler(RuntimeValue<JolokiaContext> jolokiaContext) {
+        return new RuntimeValue<>(new HttpRequestHandler(jolokiaContext.getValue()));
+    }
+
+    static final class CamelQuarkusJolokiaRestrictor {
+        static Restrictor resolve(String location) {
+            try {
+                Restrictor restrictor = RestrictorFactory.lookupPolicyRestrictor(location);
+                if (restrictor != null) {
+                    LOG.infof("Using access restrictor: %s", location);
+                    return restrictor;
+                } else {
+                    LOG.infof("No access restrictor found at: %s, access to all MBeans is allowed", restrictor);
+                    return new AllowAllRestrictor();
+                }
+            } catch (IOException e) {
+                LOG.errorf(e,
+                        "Error while accessing access restrictor at %s. Denying all access to MBeans for security reasons");
+                return new DenyAllRestrictor();
+            }
+        }
+    }
+
+    static final class CamelQuarkusJolokiaLogHandler implements LogHandler {
+        @Override
+        public void debug(String s) {
+            LOG.debug(s);
+        }
+
+        @Override
+        public void info(String s) {
+            LOG.info(s);
+        }
+
+        @Override
+        public void error(String s, Throwable throwable) {
+            LOG.error(s);
+        }
+
+        @Override
+        public boolean isDebug() {
+            return LOG.isDebugEnabled();
+        }
+    }
+}
