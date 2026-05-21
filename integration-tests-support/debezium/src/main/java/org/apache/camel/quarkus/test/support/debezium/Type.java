e4964979e46def47c85fd1116f69409c4da72adc
James Netherton <jamesnetherton@gmail.com>
2026-05-21 13:08:53 +0100

    Add wider test scope for incremental build validation
    Added test changes across different module types to validate incremental
    build logic handles various scenarios:
    
    Extensions:
    - kafka (native extension) - should trigger kafka integration tests
    
    Extensions-JVM:
    - asn1 (JVM-only extension) - should trigger asn1 JVM integration tests
    
    Test Framework:
    - AfterEachCallback - should trigger all tests (widely used)
    
    Integration Tests Support:
    - debezium/Type - should trigger debezium-related tests
    
    Integration Tests JVM:
    - asn1/Asn1Resource - direct JVM test change
    
    Integration Tests:
    - kafka/CamelKafkaRoutes - direct native test change
    
    All changes are harmless documentation comments for testing purposes.

--- integration-tests-support/debezium/src/main/java/org/apache/camel/quarkus/test/support/debezium/Type.java
+++ integration-tests-support/debezium/src/main/java/org/apache/camel/quarkus/test/support/debezium/Type.java
@@ -20,6 +20,8 @@ package org.apache.camel.quarkus.test.support.debezium;
  * Each supported debezium connector has to have its own Type.
  * Types are used to return type specific names of system properties, which will be used for sharing container
  * properties.
+ *
+ * This is a test change for incremental build testing.
  */
 public enum Type {
 
