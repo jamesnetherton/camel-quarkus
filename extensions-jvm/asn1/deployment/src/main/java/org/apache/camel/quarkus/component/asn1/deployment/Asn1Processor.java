5805a678d95f75807b2d52d138bab9200d267653
James Netherton <jamesnetherton@gmail.com>
2026-05-22 07:55:37 +0100

    Add ASN.1 JVM extension change to test integration-tests-jvm
    Added change to extensions-jvm/asn1 to test that:
    - JVM-only extension changes are detected
    - integration-tests-jvm job runs for affected modules
    - FilterCategoriesMojo correctly identifies JVM-only tests
    
    Test branch now has:
    - extensions/box (native)
    - extensions/kafka (native)
    - extensions-jvm/asn1 (JVM-only)

--- extensions-jvm/asn1/deployment/src/main/java/org/apache/camel/quarkus/component/asn1/deployment/Asn1Processor.java
+++ extensions-jvm/asn1/deployment/src/main/java/org/apache/camel/quarkus/component/asn1/deployment/Asn1Processor.java
@@ -14,6 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Build-time configuration for the ASN.1 extension (JVM-only).
+ * This is a test change for incremental build testing.
+ */
 package org.apache.camel.quarkus.component.asn1.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
