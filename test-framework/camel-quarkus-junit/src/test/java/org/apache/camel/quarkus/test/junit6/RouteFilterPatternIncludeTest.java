a4ac338bb745c3ad3c2044627f362a969f6d1f8b
James Netherton <jamesnetherton@gmail.com>
2026-06-01 14:25:20 +0100

    Test: test-framework change should NOT trigger downstream tests
    Modified test-framework to verify that infrastructure changes don't trigger
    all downstream integration tests.
    
    Expected: Scalpel detects change, but IncrementalBuildMojo filters out
    DOWNSTREAM changes from infrastructure (only DIRECT or extension changes
    should trigger tests)

--- test-framework/camel-quarkus-junit/src/test/java/org/apache/camel/quarkus/test/junit6/RouteFilterPatternIncludeTest.java
+++ test-framework/camel-quarkus-junit/src/test/java/org/apache/camel/quarkus/test/junit6/RouteFilterPatternIncludeTest.java
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Test change for infrastructure-only: should NOT trigger downstream tests
+ */
 package org.apache.camel.quarkus.test.junit6;
 
 import io.quarkus.test.junit.QuarkusTest;
